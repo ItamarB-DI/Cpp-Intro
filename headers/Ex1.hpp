796111f2f37eb4518c723bcfd7e583c516cbba10
itamar.bloch <itamar.bloch@deepinstinct.com>
2024-12-16 17:10:21 +0200

    Ex2#2 camelCase functions, access methods, returning to normal ptr

--- headers/Ex1.hpp
+++ headers/Ex1.hpp
@@ -10,29 +10,36 @@ public:
     DynamicBuffer<T>& operator=(const DynamicBuffer<T>& other); 
     DynamicBuffer<T>& operator=(DynamicBuffer<T>&& other) noexcept; 
 
-    ~DynamicBuffer() noexcept = default;
+    T& operator[](int idx);
+    T operator[](int idx) const;
+
+    ~DynamicBuffer() noexcept;
 
 private:
-    void CopyDynamicBuffer(const DynamicBuffer<T>& src);
+    void copyDynamicBuffer(const DynamicBuffer<T>& src);
+    void deAllocate();
 
     size_t m_size;
-    std::unique_ptr<T[]> m_buffer;
+    T *m_buffer;
 };
 
 /****************************************** Main Functions *****************************************/
 
 template <typename T> //Ctor
-DynamicBuffer<T>::DynamicBuffer(size_t size): m_buffer(std::make_unique<T[]>(size)), m_size(size) {}
+DynamicBuffer<T>::DynamicBuffer(size_t size): m_buffer(new T[size]), m_size(size) {}
 
 template <typename T> //CCtor
-DynamicBuffer<T>::DynamicBuffer(const DynamicBuffer<T>& other): m_buffer(std::make_unique<T[]>(other.m_size)), m_size(other.m_size) {
+DynamicBuffer<T>::DynamicBuffer(const DynamicBuffer<T>& other): m_buffer(new T[other.m_size]), m_size(other.m_size) {
 
-    CopyDynamicBuffer(other);
+    copyDynamicBuffer(other);
 }
 
 template <typename T> //Move Ctor
-DynamicBuffer<T>::DynamicBuffer(DynamicBuffer<T>&& other) noexcept : m_buffer(std::move(other.m_buffer)), m_size(other.m_size) {
+DynamicBuffer<T>::DynamicBuffer(DynamicBuffer<T>&& other) noexcept : m_buffer(new T[other.m_size]), m_size(other.m_size) {
 
+    m_buffer = other.m_buffer;
+
+    other.m_buffer = nullptr;
     other.m_size = 0;
 }
 
@@ -41,10 +48,12 @@ template <typename T> //Copy Assignment
 DynamicBuffer<T>& DynamicBuffer<T>::operator=(const DynamicBuffer<T>& other) {
 
     if (this != &other) {
-        m_buffer = std::make_unique<T[]>(other.m_size);
+        deAllocate();
+
+        m_buffer = new T[other.m_size];
         m_size = other.m_size;
 
-        CopyDynamicBuffer(other);
+        copyDynamicBuffer(other);
     }
     
     return *this;
@@ -54,22 +63,61 @@ template <typename T> //Move Assignment
 DynamicBuffer<T>& DynamicBuffer<T>::operator=(DynamicBuffer<T>&& other) noexcept {
 
     if (this != &other) {
-        m_buffer = std::move(other.m_buffer);
+
+        m_buffer = other.m_buffer;
         m_size = other.m_size;
 
+        other.m_buffer = nullptr;
         other.m_size = 0;
     }
 
     return *this;
 }
 
+template <typename T> 
+DynamicBuffer<T>::~DynamicBuffer() {
+
+    deAllocate();
+}
+
+
+template <typename T>
+T& DynamicBuffer<T>::operator[](int idx) {
+
+    if (idx < 0) {
+        throw "invalid Index";
+    }
+
+    return m_buffer[idx];
+}
+
+template <typename T>
+T DynamicBuffer<T>::operator[](int idx) const {
+
+    if (idx < 0) {
+        throw "invalid Index";
+    }
+
+    return m_buffer[idx];
+}
+
+
 /****************************************** Helper Functions *****************************************/
 
 template <typename T>
-void DynamicBuffer<T>::CopyDynamicBuffer(const DynamicBuffer<T>& src) {
+void DynamicBuffer<T>::copyDynamicBuffer(const DynamicBuffer<T>& src) {
     
     m_size = src.m_size;
     for (size_t i = 0; i < m_size; ++i) {
         m_buffer[i] = src.m_buffer[i];
     }
+}
+
+
+template <typename T>
+void DynamicBuffer<T>::deAllocate() {
+
+    delete m_buffer;
+    m_buffer = nullptr;
+    m_size = 0;
 }

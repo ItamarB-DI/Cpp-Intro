4fe3e60b390b6cb49ab1454424c3d74e882f9e93
itamar.bloch <itamar.bloch@deepinstinct.com>
2024-12-17 15:34:51 +0200

    removing redundent code & fixing vector access

--- DynamicBuffer/Ex1.hpp
+++ DynamicBuffer/Ex1.hpp
@@ -4,16 +4,9 @@ template <typename T>
 class DynamicBuffer {
 public:
     explicit DynamicBuffer(size_t size);
-    DynamicBuffer(const DynamicBuffer<T>& other);  
-    DynamicBuffer(DynamicBuffer<T> &&other) noexcept;
-
-    DynamicBuffer<T>& operator=(const DynamicBuffer<T>& other); 
-    DynamicBuffer<T>& operator=(DynamicBuffer<T>&& other) noexcept; 
 
     T& operator[](int idx);
     const T&  operator[](int idx) const;
-
-    ~DynamicBuffer() noexcept = default;
 
 private:
 
@@ -25,40 +18,14 @@ private:
 template <typename T> //Ctor
 DynamicBuffer<T>::DynamicBuffer(size_t size): m_buffer(size) {}
 
-template <typename T> //CCtor
-DynamicBuffer<T>::DynamicBuffer(const DynamicBuffer<T>& other): m_buffer(other.m_buffer) {}
-
-template <typename T> //Move Ctor
-DynamicBuffer<T>::DynamicBuffer(DynamicBuffer<T>&& other) noexcept : m_buffer(std::move(other.m_buffer)) {}
-
-
-template <typename T> //Copy Assignment
-DynamicBuffer<T>& DynamicBuffer<T>::operator=(const DynamicBuffer<T>& other) {
-
-    m_buffer = other.m_buffer;
-    
-    return *this;
-}
-
-template <typename T> //Move Assignment
-DynamicBuffer<T>& DynamicBuffer<T>::operator=(DynamicBuffer<T>&& other) noexcept {
-
-    if (this != &other) {
-
-        m_buffer = std::move(other.m_buffer);
-    }
-
-    return *this;
-}
-
 template <typename T>
 T& DynamicBuffer<T>::operator[](int idx) {
 
-    return m_buffer[idx];
+    return m_buffer.at(idx);
 }
 
 template <typename T>
 const T& DynamicBuffer<T>::operator[](int idx) const {
 
-    return m_buffer[idx];
+    return m_buffer.at(idx);
 }

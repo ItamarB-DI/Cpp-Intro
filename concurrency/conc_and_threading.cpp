7446d89ac1eb9881c6b49275e313e1ce6634be50
itamar.bloch <itamar.bloch@deepinstinct.com>
2024-12-23 10:23:57 +0200

    establishing the communication between the threads

--- concurrency/conc_and_threading.cpp
+++ concurrency/conc_and_threading.cpp
@@ -2,25 +2,27 @@
 #include <filesystem>
 #include <future>
 #include <string>
+#include <vector>
+#include <fstream> 
 
 
-void clientJob(std::promise<std::string> file_name, std::future<std::string> what_the_server_read);
-void serverJob(std::future<std::string> client_file_name, std::promise<std::string> promise_file_data);
+void clientJob(std::promise<std::filesystem::path> file_name, std::future<std::vector<char>> what_the_server_read);
+void serverJob(std::future<std::filesystem::path> client_file_name, std::promise<std::vector<char>> promise_file_data);
 
-void readFileName(std::promise<std::string> file_name);
-void readFromFile(std::promise<std::string> promise_file_data);
+void readFileName(std::promise<std::filesystem::path>& file_name);
+void readFileContent(std::promise<std::vector<char>>& promise_file_data, std::filesystem::path& file_name);
 
 int main() {
     
     // create packed_task for the file name (the clientJob would get the promise & serverJob would get the future)
     // create packed_task for the file content (the clientJob would get the future & serverJob would get the promise) 
 
-    std::promise<std::string> promise_file_name;
-    std::future<std::string> future_file_name = promise_file_name.get_future();
+    std::promise<std::filesystem::path> promise_file_name;
+    std::future<std::filesystem::path> future_file_name = promise_file_name.get_future();
 
 
-    std::promise<std::string> promise_file_content;
-    std::future<std::string> future_file_content = promise_file_content.get_future();
+    std::promise<std::vector<char>> promise_file_content;
+    std::future<std::vector<char>> future_file_content = promise_file_content.get_future();
 
     // create thread with client job
     // create thread with server job
@@ -33,29 +35,66 @@ int main() {
     // join server
 
     client_thread.join();
-
+    server_thread.join();
 
     return 0;
 }
 
 
-void clientJob(std::promise<std::string> file_name, std::future<std::string> what_the_server_read) {
-    (void)file_name;
-    (void)what_the_server_read;
+void clientJob(std::promise<std::filesystem::path> file_name, std::future<std::vector<char>> what_the_server_read) {
 
-    // read file name from the cli and fill the promise 
+    readFileName(file_name);
 
-    // read the buffer from the promise 
-    // print the buffer content 
+    std::vector<char> file_content = what_the_server_read.get();
+    std::cout << std::endl;
+    for (long unsigned int i = 0; i < file_content.size(); ++i) {
+        std::cout << file_content[i];
+    }
+    std::cout << std::endl;
+}
+
+void serverJob(std::future<std::filesystem::path> client_file_name, std::promise<std::vector<char>> promise_file_data) {
+
+    auto file_name = client_file_name.get();
+
+    readFileContent(promise_file_data, file_name);
+}
+
+void readFileName(std::promise<std::filesystem::path>& file_name) {
+
+    std::filesystem::path path;
+
+    try {
+        std::cout << "Please Enter File Name: " << std::endl;
+        std::cin >> path;
+        
+        file_name.set_value(path);
+    } catch (std::ios_base::failure &e) {
+        auto eptr = std::current_exception();
+        file_name.set_exception(eptr);
+    }
 
 }
 
-void serverJob(std::future<std::string> client_file_name, std::promise<std::string> promise_file_data) {
-    (void)client_file_name;
-    (void)promise_file_data;
-    // read the file name from the promise 
+void readFileContent(std::promise<std::vector<char>>& promise_file_data, std::filesystem::path& file_name) {
 
-    // read the content of the file and put it in the future (inside a buffer)
+    // ++handle exceptions 
+
+    try {
+        std::fstream file(file_name, std::ios::in);
+        std::istreambuf_iterator<char> it(file), eof;
+        std::vector<char> data;
+
+        while (it != eof) {
+            data.push_back(*it);
+            ++it;
+        }
+
+        promise_file_data.set_value(data);
+    } catch (std::exception& e) {
+        auto eptr = std::current_exception();
+        promise_file_data.set_exception(eptr);
+    }
 
 }
 
